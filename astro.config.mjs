66c161a913a4f08083c4f104d818dbabc890a906
Cristhian Agamez <cristhian_agamez@outlook.com>
2025-09-12 15:51:36 +0200

    Implementing supabase auth system 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     vite: {
+        // @ts-ignore
         plugins: [tailwindcss()]
-    }
+    },
+    output: 'server',
 });
